3a973e97af5316a97b342d9ad6b960dee7cebfec
Pelznotion <145939369+Pelznotion@users.noreply.github.com>
2024-06-17 13:19:35 -1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const modalDialog = document.querySelector(".copy-link-dialog");
+const shareBtn = document.querySelector(".share-btn");
+const closedBtn = document.querySelector(".close-btn");
+const copyBtn = document.querySelector(".copy-btn");
+
+shareBtn.addEventListener("click", () => {
+  modalDialog.classList.remove("copy-link-dialog--fadeout");
+  modalDialog.showModal();
+});
+closedBtn.addEventListener("click", () => {
+  modalDialog.classList.add("copy-link-dialog--fadeout");
+  modalDialog.close();
+});
+copyBtn.addEventListener("click", () => {
+  const copyInput = document.getElementById("copy-link-input");
+
+  copyInput.select();
+  copyInput.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyInput.value);
+  const copyText = document.getElementById("copy-text");
+  copyText.innerHTML = "Copied";
+});
